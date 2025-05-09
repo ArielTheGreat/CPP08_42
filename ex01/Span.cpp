8d78684b39463f06e9fe3eda046f725d83577138
frocha <frocha@student.42heilbronn.de>
2025-05-09 21:46:24 +0200

    added Span definition in cpp

---
+++ ex01/Span.cpp
@@ -0,0 +1,40 @@
+#include "Span.hpp"
+
+Span::Span()
+{
+
+}
+
+Span::Span(unsigned int maxSize)
+{
+
+}
+Span::Span(const Span& other)
+{
+
+}
+
+Span& Span::operator=(const Span& other)
+{
+
+}
+
+Span::~Span(){}
+
+void Span::addNumber(unsigned int num)
+{
+
+}
+
+void Span::addNumber(unsigned int begin, unsigned int end)
+{
+
+}
+unsigned int Span::shortestSpan()
+{
+
+}
+unsigned int Span::longestSpan()
+{
+
+}
