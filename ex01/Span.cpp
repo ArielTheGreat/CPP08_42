5451b255042aee89692b20a14a94e55a45f1a860
frocha <frocha@student.42heilbronn.de>
2025-05-09 22:41:14 +0200

    added implementation for Span member functions and also added exception messages

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,40 +1,54 @@
 #include "Span.hpp"
 
-Span::Span()
+Span::Span() : _limitNumberSize(100)
 {
 
 }
 
-Span::Span(unsigned int maxSize)
+Span::Span(unsigned int maximumSize) : _limitNumberSize(maximumSize)
 {
 
 }
-Span::Span(const Span& other)
+Span::Span(const Span& other) :  _container(other._container), _limitNumberSize(other._limitNumberSize)
 {
 
 }
 
 Span& Span::operator=(const Span& other)
 {
-
+    if (this != &other)
+	{
+		_limitNumberSize = other._limitNumberSize;
+		_container = other._container;
+	}
+	return (*this);
 }
 
 Span::~Span(){}
 
 void Span::addNumber(unsigned int num)
 {
-
+    if (_container.size() >= _limitNumberSize)
+        throw LimitReached();
+    _container.push_back(num);
 }
 
 void Span::addNumber(unsigned int begin, unsigned int end)
 {
+    if (begin >= end || _container.size() + (end - begin) > _limitNumberSize)
+		throw RangeLimitSurpassed();
 
+    for (unsigned int i = begin; i < end; ++i) {
+		_container.push_back(i);
+	}
 }
 unsigned int Span::shortestSpan()
 {
-
+    if (_container.size() <= 2)
+		throw NotEnoughNumbers();
 }
 unsigned int Span::longestSpan()
 {
-
+    if (_container.size() <= 2)
+		throw NotEnoughNumbers();
 }
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -16,6 +16,30 @@ class Span
         Span(const Span& other);
         Span& operator=(const Span& other);
         ~Span();
+
+        class NotEnoughNumbers : public std::exception 
+        {
+            public:
+				const char* what() const noexcept override {
+					return "Not enough elements to calculate span!";
+				}
+        };
+
+        class LimitReached : public std::exception 
+        {
+            public:
+				const char* what() const noexcept override {
+					return "Cannot add more elements, limit of container has been reached!";
+				}
+        };
+
+        class RangeLimitSurpassed : public std::exception 
+        {
+            public:
+				const char* what() const noexcept override {
+					return "Operation not valid, since it will surpass the limit";
+				}
+        };
         
         void addNumber(unsigned int num);
         void addNumber(unsigned int begin, unsigned int end);
