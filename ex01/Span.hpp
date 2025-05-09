d6b72e77d3ec8b826dd6fae6e90ca8f8d7f5b226
frocha <frocha@student.42heilbronn.de>
2025-05-09 21:18:05 +0200

    ex01 span header added

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,9 +1,24 @@
 #ifndef SPAN_CPP
 #define SPAN_CPP
 
+#include <iostream>
+
 class Span
 {
-    
+    private:
+
+    public:
+        //Orthodox canonical form
+        Span();
+        Span(unsigned int maxSize);
+        Span(const Span& other);
+        Span& operator=(const Span& other);
+        ~Span();
+        
+        void addNumber(unsigned int num);
+        void addNumber(unsigned int begin, unsigned int end);
+        unsigned int shortestSpan();
+        unsigned int longestSpan();
 };
 
 #endif
